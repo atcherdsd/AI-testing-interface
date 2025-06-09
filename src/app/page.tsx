f8a7340e9d4c7f99ac8d9d8448a9028ed761d36c
atcherdsd <tchernak.csg@gmail.com>
2025-06-09 10:18:30 +0300

    feat: optimize welcome page loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import PageData from '@/data/index.json';
 import Button from './components/ui/Button/Button';
 import css from './page.module.scss';
-import SafeHtml from './components/common/SafeHtml/SafeHtml';
 
 export default function WelcomePage() {
     const { title, startButton } = PageData.welcomePage;
@@ -9,7 +8,11 @@ export default function WelcomePage() {
     return (
         <main className={css.root}>
             <div className={css.rootContent}>
-                <SafeHtml className={`heading ${css.rootTitle}`} html={title} as="h1" />
+                <h1 className={`heading ${css.rootTitle}`}>
+                    {title.map((line) => (
+                        <span className={css.rootTitleLine} key={line.text}>{line.text}</span>
+                    ))}
+                </h1>
                 <Button className={`start-button ${css.rootButton}`} {...startButton} />
             </div>
         </main>
